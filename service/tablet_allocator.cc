acf05b6910beaf912cbb0a5f0fb31d7cfbf34d9e
Ferenc Szili <ferenc.szili@scylladb.com>
2025-06-27 17:46:28 +0200

    load_balancer: add logging of node and shard load

--- service/tablet_allocator.cc
+++ service/tablet_allocator.cc
@@ -531,6 +531,16 @@ class load_balancer {
     // Data structure used for making load-balancing decisions over a set of nodes.
     using node_load_map = std::unordered_map<host_id, node_load>;
 
+    void dump(const node_load_map& nlm, sstring reason = "") {
+        lblogger.info("--- nodes {}", reason);
+        for (const auto& [host, nl] : nlm) {
+            lblogger.info(" node: {} tablets: {} du: {}", host, nl.tablet_count, nl.dusage);
+            for (shard_id id = 0; id < nl.shards.size(); id++) {
+                lblogger.info("  shard: {} tablets: {} du: {}", id, nl.shards[id].tablet_count, nl.shards[id].dusage);
+            }
+        }
+    }
+
     // Less-comparator which orders nodes by load.
     struct nodes_by_load_cmp {
         node_load_map& nodes;
@@ -3274,6 +3284,8 @@ public:
                          host, dc, load.rack(), load.avg_load, load.tablet_count, load.shard_count,
                          load.tablets_per_shard(), load.state(), load.dusage->capacity, read, write);
         }
+
+        dump(nodes, "after compute imbalance");
 
         if (!nodes_to_drain.empty() || (_tm->tablets().balancing_enabled() && (shuffle || !is_balanced(min_load, max_load)))) {
             host_id target = *min_load_node;
