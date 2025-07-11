486daa9f54260d3f657c6961db558450f336a4a6
Ferenc Szili <ferenc.szili@scylladb.com>
2025-07-11 12:51:52 +0200

    logging: add table load per node

--- service/tablet_allocator.cc
+++ service/tablet_allocator.cc
@@ -3287,6 +3287,30 @@ public:
 
         dump(nodes, "after compute imbalance");
 
+        // log table load
+        for (auto table : std::views::keys(_tm->tablets().all_table_groups())) {
+            lblogger.debug("-- table load for {}", table);
+            for (const auto& [host, n] : nodes) {
+                locator::disk_usage node_du {n.dusage->capacity, 0};
+                size_t node_tablet_count = 0;
+                if (n.tablet_sizes_per_table.contains(table)) {
+                    node_du.used = n.tablet_sizes_per_table.at(table);
+                    node_tablet_count = n.tablet_count_per_table.at(table);
+                }
+                lblogger.debug(" node: {} tablets: {} load: {}", n.id, node_tablet_count, node_du);
+                for (shard_id sid = 0; sid < n.shards.size(); sid++) {
+                    const auto& shard_load = n.shards[sid];
+                    locator::disk_usage shard_du { shard_load.dusage->capacity, 0 };
+                    size_t shard_tablet_count = 0;
+                    if (shard_load.tablet_sizes_per_table.contains(table)) {
+                        shard_du.used = shard_load.tablet_sizes_per_table.at(table);
+                        shard_tablet_count = shard_load.tablet_count_per_table.at(table);
+                    }
+                    lblogger.debug("  shard: {} tablets: {} load: {}", sid, shard_tablet_count, shard_du);
+                }
+            }
+        }
+
         if (!nodes_to_drain.empty() || (_tm->tablets().balancing_enabled() && (shuffle || !is_balanced(min_load, max_load)))) {
             host_id target = *min_load_node;
             lblogger.info("target node: {}, avg_load: {}, max: {}", target, min_load, max_load);
