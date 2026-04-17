957130c375eb0ceb92f2d40299d1357330fbb233
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2026-04-17 15:15:11 +0200

    gms: introduce AUTO_REPLICATION_FACTOR cluster feature

--- gms/feature_service.hh
+++ gms/feature_service.hh
@@ -198,6 +198,7 @@ public:
     gms::feature small_table_optimization_size_probe { *this, "SMALL_TABLE_OPTIMIZATION_SIZE_PROBE"sv };
     gms::feature alternator_composite_gsi_keys { *this, "ALTERNATOR_COMPOSITE_GSI_KEYS"sv };
     gms::feature cluster_config_registry_v0 { *this, "CLUSTER_CONFIG_REGISTRY_V0"sv };
+    gms::feature auto_replication_factor { *this, "AUTO_REPLICATION_FACTOR"sv };
 public:
 
     const std::unordered_map<sstring, std::reference_wrapper<feature>>& registered_features() const;
