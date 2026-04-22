7fd1e53c0f6656bb0c4ee979578dc6b7d3389a48
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2026-04-22 16:42:03 +0200

    use prepare_options in table_helper::setup_keyspace

--- cql3/statements/ks_prop_defs.cc
+++ cql3/statements/ks_prop_defs.cc
@@ -119,7 +119,7 @@ expand_to_racks(const locator::token_metadata& tm,
     return dc_racks;
 }
 
-static locator::replication_strategy_config_options prepare_options(
+locator::replication_strategy_config_options prepare_options(
         const sstring& strategy_class,
         const locator::token_metadata& tm,
         bool rf_rack_valid_keyspaces,
